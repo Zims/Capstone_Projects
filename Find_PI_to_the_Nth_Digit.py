f79942fa44236bb644ceb91c33ff0f0966deb178
Zims <zims@inbox.lv>
2020-03-17 20:23:33 +0200

    need if int test

--- Find_PI_to_the_Nth_Digit.py
+++ Find_PI_to_the_Nth_Digit.py
@@ -1,10 +1,15 @@
+
+
+
 pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286
-after_comma = int(input(f"How many numbers after the dot do you want? "))
 
-def upTo(after_comma):
+
+def upTo():
+    after_comma = int(input(f"How many numbers after the dot do you want? "))
+
+
     result = round(pi, after_comma)
-    print(result)
+    print(f'Here is pi with {after_comma} numbers after the dot: {result}')
 
 
-
-upTo(after_comma)
+upTo()
