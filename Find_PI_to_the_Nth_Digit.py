1b154e73538cba78c383233fcfa419ec4a36eaa5
Zims <zims@inbox.lv>
2020-03-17 19:58:36 +0200

    rough(works)

--- Find_PI_to_the_Nth_Digit.py
+++ Find_PI_to_the_Nth_Digit.py
@@ -1,3 +1,10 @@
 pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286
-def upTo(n):
-    
+after_comma = int(input(f"How many numbers after the dot do you want? "))
+
+def upTo(after_comma):
+    result = round(pi, after_comma)
+    print(result)
+
+
+
+upTo(after_comma)
