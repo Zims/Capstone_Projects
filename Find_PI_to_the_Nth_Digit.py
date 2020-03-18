646a6798a3834ebee698e49212af89aa7e29870b
Zims <zims@inbox.lv>
2020-03-18 11:37:38 +0200

    with int check and restart

--- Find_PI_to_the_Nth_Digit.py
+++ Find_PI_to_the_Nth_Digit.py
@@ -5,13 +5,17 @@
 
 pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286
 
-
 def upTo():
-    after_comma = int(input(f"How many numbers after the dot do you want? "))
+    after_comma = None
 
-
-    result = round(pi, after_comma)
-    print(f'Here is pi with {after_comma} numbers after the dot: {result}')
-
+    while type(after_comma) != int:
+        try:
+            after_comma = int(input(f"How many numbers after the dot do you want? "))
+        except ValueError:
+                    print('Sorry, a bet must be an integer!')
+        else:
+            result = round(pi, after_comma)
+            print(f'Here is pi with {after_comma} numbers after the dot: {result}')
+            upTo()
 
 upTo()
