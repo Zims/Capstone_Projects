f6198ec1ac643db7d29ffe5f06af8cea3f25e4e4
Zims <zims@inbox.lv>
2020-03-18 19:06:23 +0200

    done

--- Prime_Factorization.py
+++ Prime_Factorization.py
@@ -3,24 +3,27 @@
 
 # https://www.khanacademy.org/math/pre-algebra/pre-algebra-factors-multiples/pre-algebra-prime-factorization-prealg/v/prime-factorization-exercise
 
-prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-factor_num = int(input('I want to do prime factorization on: '))
-factor_list = []
+factors = lambda n: [x for x in range(1,n+1) if not n%x]
+is_prime = lambda n: len(factors(n))==2
+primefactors = lambda n: list(filter(is_prime ,factors(n)))
 
+def primeFactorize(n):
+	n = int(n)
+	f = primefactors(n)
+	if is_prime(n):
+		return str(n)
+	else:
+		return str(f[0])+ "*"+primeFactorize(n/f[0])
 
-def loop():
-    for prime in prime_list:
-        if factor_num % prime == 0:
-            factor_list.append(prime)
-
-
-            
-def factorize():
-    if factor_num not in prime_list:
-        loop()
-
-    else:
-        print(f'{factor_num} is prime AF')
-
-factorize()
-print(factor_list)
+if __name__=='__main__':
+	print ("Welcome to the Prime Factorizer.. Enter the numbers in the prompt or enter 'quit' to exit")
+	num=0;
+	
+	while True:
+		if num:
+			print(primeFactorize(num))
+		print(">>>",end='')
+		num=input()
+		if num=="quit":
+			break
+		
