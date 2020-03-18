28a1b9ee35ab34be4ebe588ec7e38aee16a4bc67
Zims <zims@inbox.lv>
2020-03-18 12:39:49 +0200

    Find e to the Nth Digit - complete

--- Find_e_to_the_Nth_Digit.py
+++ Find_e_to_the_Nth_Digit.py
@@ -3,3 +3,20 @@
 # but with e instead of π (pi). Enter a number and have the program generate e 
 # up to that many decimal places. Keep a limit to how far the program will go.
 
+import random
+
+
+def upTo():
+    after_comma = None
+
+    while type(after_comma) != int:
+        try:
+            after_comma = int(input(f"How many numbers after the dot do you want? "))
+        except ValueError:
+                    print('Sorry, a bet must be an number!')
+        else:
+            result = round(random.random(), after_comma)
+            print(f'Here is pi with {after_comma} numbers after the dot: {result}')
+            upTo()
+
+upTo()
