addc2f84d54b764eada9d63390cb3583a5fad3a7
Zims <zims@inbox.lv>
2020-03-25 22:04:12 +0200

    done

---
+++ count_vowels.py
@@ -0,0 +1,18 @@
+# * Count Vowels - Enter a string and the program counts the number of vowels in the text. 
+# For added complexity have it report a sum of each vowel found.
+from collections import Counter
+
+user_input = input('Write a text and I will count the vowels: ')
+# print(f'You chose this text: {user_input}')
+lowecase_input = user_input.lower()
+
+def vowel_counter(lowecase_input):
+
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    final = [each for each in lowecase_input if each in vowel]
+    
+    print(f'I counted {len(final)} vowels!')
+    print(final)
+    print(Counter(final))
+
+vowel_counter(lowecase_input)ß
