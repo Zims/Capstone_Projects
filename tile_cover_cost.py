42898a95d91fa93b0ad2b8e1ac122bb75ca6b867
Zims <zims@inbox.lv>
2020-03-27 16:47:00 +0200

    easy done

--- tile_cover_cost.py
+++ tile_cover_cost.py
@@ -1,27 +1,33 @@
-# Find Cost of Tile to Cover W x H Floor - 
-# Calculate the total cost of tile it would take to cover a floor plan of width and height, 
-# using a cost entered by the user. Visual or ui??? How to calculate the useless cuts?
+# Find Cost of Tile to Cover W x H wall - 
+# Calculate the total cost of tile it would take to cover a room, 
+# using a cost entered by the user. Visual or ui???
 
 # fitst calculate for a room 2x400*220cm and 2x300*220cm. Tile = 20*30cm. Price per m**2 = x. 
 # Calculate needed m**2 to cover room. 
 
-# Check length and width vs full tile count + count of tiles that are cut.
+# CRATE x y axes input
+# Advanced: Check length and width vs full tile count + count of tiles that are cut. 
+# Or sq/m of full tiles + extra 1 for each row and collumn - 1
 
 
 # Room size --
-big_width = (400, 220)
-short_width = (300, 220)
-tile_size = (20, 30)
+big_width = (4.0, 2.2)
+short_width = (3.0, 2.2)
+tile_size = (0.2, 0.3)
+extra_tile = 1.15
 
 
 def get_price():
+    price_sqm = None
     while type(price_sqm) != int or float:
         try:
-            price_sqm = float(input('Price per sqm? ')):
+            price_sqm = float(input('Price per sqm? '))
         except ValueError:
-            print('Sorry, a bet must be an number!')
+            print('Sorry, price in USD!')
         else:
-        if type(price_sqm) != int or float:
+
+            whole_price = price_sqm * whole_area
+            print(f'The whole price is {whole_price} USD')
 
 
 def sq_area(gabarites):
@@ -30,12 +36,22 @@ def sq_area(gabarites):
 def combined_sq_area(big_wall, small_wall):
     return big_wall*2 + small_wall*2
 
-big_wall = sq_area(big_width)
-small_wall = sq_area(short_width)
-tile_area = sq_area(tile_size)
-whole_area = combined_sq_area(big_wall, small_wall)
+big_wall = round(sq_area(big_width), 2)
 
-print(big_wall)
-print(small_wall)
-print(tile_area)
-print(whole_area)
+small_wall = round(sq_area(short_width), 2)
+
+tile_area = round(sq_area(tile_size), 3)
+
+whole_area = round(combined_sq_area(big_wall, small_wall), 2)
+
+should_buy = round(whole_area * extra_tile, 2)
+
+
+
+# print(big_wall)
+# print(small_wall)
+# print(tile_area)
+print(f'The whole area is {whole_area} sq/m')
+print(f'To be on the safe side you should buy 15% extra tile. That\'s {should_buy} sq/m')
+
+get_price()
