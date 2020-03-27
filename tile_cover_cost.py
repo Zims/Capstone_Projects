8024f6e707eb647535da2b48a02472ce40ef3b41
Zims <zims@inbox.lv>
2020-03-27 13:44:04 +0200

    fixed sq

--- tile_cover_cost.py
+++ tile_cover_cost.py
@@ -5,22 +5,37 @@
 # fitst calculate for a room 2x400*220cm and 2x300*220cm. Tile = 20*30cm. Price per m**2 = x. 
 # Calculate needed m**2 to cover room. 
 
+# Check length and width vs full tile count + count of tiles that are cut.
+
 
 # Room size --
-height = 220
-big_width = 400
-short_width = 300
+big_width = (400, 220)
+short_width = (300, 220)
+tile_size = (20, 30)
 
-def wall_sq_area(height, width):
-    return height * width 
+
+def get_price():
+    while type(price_sqm) != int or float:
+        try:
+            price_sqm = float(input('Price per sqm? ')):
+        except ValueError:
+            print('Sorry, a bet must be an number!')
+        else:
+        if type(price_sqm) != int or float:
+
+
+def sq_area(gabarites):
+    return gabarites[0] * gabarites[1]
 
 def combined_sq_area(big_wall, small_wall):
     return big_wall*2 + small_wall*2
 
-big_wall = wall_sq_area(height, big_width)
-small_wall = wall_sq_area(height, short_width)
+big_wall = sq_area(big_width)
+small_wall = sq_area(short_width)
+tile_area = sq_area(tile_size)
 whole_area = combined_sq_area(big_wall, small_wall)
 
 print(big_wall)
 print(small_wall)
+print(tile_area)
 print(whole_area)
