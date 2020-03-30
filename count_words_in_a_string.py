bc425d615c2a4df946d176e33f840ad55276c926
Zims <zims@inbox.lv>
2020-03-30 14:04:06 +0300

    easy way

---
+++ count_words_in_a_string.py
@@ -0,0 +1,8 @@
+# * Count Words in a String - Counts the number of individual words in a string. 
+# For added complexity read these strings in from a text file and generate a summary.
+
+
+given_string = 'this is so heavy'
+
+string_to_list = given_string.split()
+print(len(string_to_list))
